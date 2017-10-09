238634747670b3c1470963219ae5d92db078ec62
mvdrummer95 <njvanstee@gmail.com>
2017-10-09 19:28:27 -0400

    This is a new file

---
+++ 2DGame.py
@@ -0,0 +1,14 @@
+#from kivy.app import app
+#from kivy.uix.Button import button
+from kivy.core.window import Window
+
+Window.fullscreen = True
+
+#root = FloatLayout()
+#b1 = app.button(pos_hint={'x': 0, 'center_y': .5})
+#b2 = app.button(pos_hint={'right': 1, 'center_y': .5})
+
+
+class App(app):
+    """Docstring."""
+    pass
