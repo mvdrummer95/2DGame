15248e11c650d0e911f44baa393c9bd35aa74a05
mvdrummer95 <njvanstee@gmail.com>
2017-10-16 19:21:31 -0400

    second commit

--- 2DGame.py
+++ 2DGame.py
@@ -1,14 +1,24 @@
-#from kivy.app import app
-#from kivy.uix.Button import button
+import kivy 
+kivy.require('1.10.0')
+from kivy import app
+from kivy.uix import button as button
 from kivy.core.window import Window
+from kivy import Config
+from kivy import clock
+from kivy import app
 
-Window.fullscreen = True
-
-#root = FloatLayout()
-#b1 = app.button(pos_hint={'x': 0, 'center_y': .5})
-#b2 = app.button(pos_hint={'right': 1, 'center_y': .5})
+clock.max_iteration = 20
+Config.set('graphics', 'multisamples', '0')
+x = 2
 
 
-class App(app):
-    """Docstring."""
-    pass
+b1 = button.Button(pos_hint={'x': 0, 'center_y': .5})
+b2 = button.Button(pos_hint={'right': 1, 'center_y': .5})
+#class TestApp(app):
+#    pass
+
+while x > 1:
+	Window.fullscreen = True
+
+#if __name__ == '__main__':
+#    Testapp
