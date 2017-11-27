2827c379fa97b6fd02e3cccf8c68da492c1fb5a8
Nate VanStee <njvanstee@gmail.com>
2017-11-27 16:52:30 -0500

    11/27/17

--- 2DGame.py
+++ 2DGame.py
@@ -1,24 +1,21 @@
-import kivy 
-kivy.require('1.10.0')
-from kivy import app
-from kivy.uix import button as button
-from kivy.core.window import Window
-from kivy import Config
-from kivy import clock
-from kivy import app
+"""RollerGame.py."""
+from kivy.uix.floatlayout import FloatLayout
+from kivy.app import App
+from kivy.lang import Builder
 
-clock.max_iteration = 20
-Config.set('graphics', 'multisamples', '0')
-x = 2
+Builder.load_file('2DGame.kv')
 
 
-b1 = button.Button(pos_hint={'x': 0, 'center_y': .5})
-b2 = button.Button(pos_hint={'right': 1, 'center_y': .5})
-#class TestApp(app):
-#    pass
+class Controls(FloatLayout):
+    pass
 
-while x > 1:
-	Window.fullscreen = True
 
-#if __name__ == '__main__':
-#    Testapp
+class RollerApp(App):
+    """Main app."""
+    def build(self):
+        """."""
+        return Controls()
+
+
+if __name__ == '__main__':
+    RollerApp().run()
