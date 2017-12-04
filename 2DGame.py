fd2f599fc99e8517ca48545d441ff4cdb511b16e
Nate VanStee <njvanstee@gmail.com>
2017-12-04 16:58:34 -0500

    full screen buttons

--- 2DGame.py
+++ 2DGame.py
@@ -6,15 +6,37 @@ from kivy.lang import Builder
 Builder.load_file('2DGame.kv')
 
 
-class Controls(FloatLayout):
-    pass
-
 
 class RollerApp(App):
     """Main app."""
     def build(self):
         """."""
         return Controls()
+
+
+
+class Controls(FloatLayout):
+    def Test_Function1(self):
+        self.ids['1'].background_color = 0, 0.0, 0.0, 1.0
+        self.ids['2'].background_color = 0, 0.0, 0.0, 1.0
+        self.ids['3'].background_color = 0, 0.0, 0.0, 1.0
+        self.ids['4'].background_color = 0, 0.0, 0.0, 1.0
+    def Test_Function2(self):
+        self.ids['1'].background_color = 0, 0.0, 1.0, 1.0
+        self.ids['2'].background_color = 0, 0.0, 1.0, 1.0
+        self.ids['3'].background_color = 0, 0.0, 1.0, 1.0
+        self.ids['4'].background_color = 0, 0.0, 1.0, 1.0
+    def Test_Function3(self):
+        self.ids['1'].background_color = 0, 1.0, 0.0, 1.0
+        self.ids['2'].background_color = 0, 1.0, 0.0, 1.0
+        self.ids['3'].background_color = 0, 1.0, 0.0, 1.0
+        self.ids['4'].background_color = 0, 1.0, 0.0, 1.0
+    def Test_Function4(self):
+        self.ids['1'].background_color = 1, 0.0, 0.0, 1.0
+        self.ids['2'].background_color = 1, 0.0, 0.0, 1.0
+        self.ids['3'].background_color = 1, 0.0, 0.0, 1.0
+        self.ids['4'].background_color = 1, 0.0, 0.0, 1.0
+    pass
 
 
 if __name__ == '__main__':
